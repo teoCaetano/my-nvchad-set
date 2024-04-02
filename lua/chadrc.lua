bf6288ada24df0624ca157ac73d2669323e49c2b
Teo Caetano Molina <123020349+teoCaetano@users.noreply.github.com>
2024-04-02 11:18:22 -0300

    new config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,8 +1,11 @@
----@type ChadrcConfig
+-- This file  needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+--@type ChadrcConfig
+
 local M = {}
 
 M.ui = {
-	theme = "onedark",
+	theme = "gruvbox",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,34 +1,46 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre' -- uncomment for format on save
+    -- event = 'BufWritePre', -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
   },
+
   {
-     "neovim/nvim-lspconfig",
-     config = function()
-       require("nvchad.configs.lspconfig").defaults()
-       require "configs.lspconfig"
-     end,
+   "neovim/nvim-lspconfig",
+   config = function()
+     require("nvchad.configs.lspconfig").defaults()
+     require "configs.lspconfig"
+   end,
   },
+
   {
-   	"williamboman/mason.nvim",
-   	opts = {
-   		ensure_installed = {
-   			"lua-language-server", "stylua",
-   			"html-lsp", "css-lsp" , "prettier"
-   	 },
-   	},
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "lua-language-server",
+      "stylua",
+      "prettier",
+      "clangd",
+      "arduino-language-server",
+      "cmake-language-server"
+
+    },
   },
+  },
+
   {
-   	  "nvim-treesitter/nvim-treesitter",
-   	  opts = {
-   		  ensure_installed = {
- 		      "vim", "lua", "vimdoc",
-          "html", "css", "c"
-    	  },
-      },
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "vim",
+      "lua",
+      "vimdoc",
+      "c",
+      "cpp",
+      "cmake"
+    },
+  },
   },
 }
